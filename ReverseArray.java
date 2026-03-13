e954854bd0095a385e85d363c1345b94dcb4f504
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-13 16:36:08 +0530

    Added reverse array program (SwapArray)

---
+++ ReverseArray.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class ReverseArray{
+  public static void main(String[]args){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the Size of an Array:");
+    int size = sc.nextInt();
+
+    if(size<=0){
+            System.out.print("Enter Valid Size!");
+            sc.close();
+            return;
+    }
+
+
+    int []arr=new int[size];
+
+    for(int i=0;i<arr.length;i++){
+      System.out.print("Enter Element Number "+(i+1)+ " :");
+      arr[i]=sc.nextInt();
+    }
+
+    int start = 0;
+    int end = arr.length-1;
+
+    
+
+    while(start<end){
+      int temp = arr[start];
+      arr[start]=arr[end];
+      arr[end]=temp;
+
+      start++;
+      end--;
+    }
+      System.out.print("Reverse Order of Array Is : ");
+
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]+" ");
+    }
+
+    sc.close();
+  }
+}
