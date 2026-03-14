3f99293e44ed7cdcbf47e4ff0facd1c361b7267c
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-14 20:30:27 +0530

    Upgrade find max number in array with user input

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,15 +1,27 @@
-public class LargestElement {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        int arr[] = {10, 25, 8, 99, 54};
-        int max = arr[0];
+public class MaxNum0fArray {
+    public static void main(String[]args){
+       Scanner sc = new Scanner(System.in);
 
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
+       System.out.print("Enter the Size An Array:");
+       int size=sc.nextInt();
+
+       int [] arr = new int [size];
+       
+       for(int i=0;i<arr.length;i++){
+           System.out.print("Enter Num of Elements " + (1+i) + ":" );
+           arr[i]=sc.nextInt();
         }
+        
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+        if(arr[i]>max){
+           max= arr[i];
+        }
+       }
+        System.out.print("Max Elements Is:"+max);
 
-        System.out.println("Largest Element: " + max);
+sc.close();
     }
 }
