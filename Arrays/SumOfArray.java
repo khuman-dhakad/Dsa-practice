e8cea895b6d8675e2d870f580bb72b8ea5cb2759
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-18 19:21:06 +0530

    Added clean ouput in program

--- Arrays/SumOfArray.java
+++ Arrays/SumOfArray.java
@@ -7,7 +7,6 @@ public class SumOfArray {
         System.out.print("Enter the Size of an Array:");
         int size = sc.nextInt();
 
-        
         if (size <= 0) {
             System.out.println("Enter Valid Size!");
             sc.close();
@@ -15,19 +14,19 @@ public class SumOfArray {
         }
         int[] arr = new int[size];
 
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Enter Element Number " + (i + 1) + " :");
             arr[i] = sc.nextInt();
+            sum += arr[i];
+
         }
 
-        int sum = 0;
-         System.out.print("Element Of Array Is: ");
+        System.out.print("Element Of Array Is: ");
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(+arr[i]+" ");
-            sum = sum + arr[i];
+            System.out.print(arr[i] + " ");
         }
-        System.out.println("\nThe Sum of the Array =" + sum);
-        
+        System.out.println("\nSum of array = " + sum);
         sc.close();
     }
 }
