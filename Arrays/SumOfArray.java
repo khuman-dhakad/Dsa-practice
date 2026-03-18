37913ff75d996fb3cd233e5e7e1763d334422430
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-18 19:09:13 +0530

    Added case handling for size of an array

--- Arrays/SumOfArray.java
+++ Arrays/SumOfArray.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
-public class SumOfArray{
-    public static void main(String[]args){
-       Scanner sc = new Scanner(System.in);
 
-       System.out.print("Enter the Size of an Array:");
-       int size = sc.nextInt();
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-       int []arr= new int [size];
+        System.out.print("Enter the Size of an Array:");
+        int size = sc.nextInt();
 
-       for(int i=0;i<arr.length;i++){
-        System.out.print("Enter Element Number "+(i+1)+" :");
-        arr[i]= sc.nextInt();
-       }
+        
+        if (size <= 0) {
+            System.out.println("Enter Valid Size!");
+            sc.close();
+            return;
+        }
+        int[] arr = new int[size];
 
-       int sum=0;
-    for(int i=0;i<arr.length;i++){
-        sum=sum+arr[i];
-    }
-    System.out.println("The Sum of the Array ="+sum);
-    sc.close();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter Element Number " + (i + 1) + " :");
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("The Sum of the Array =" + sum);
+        sc.close();
     }
 }
