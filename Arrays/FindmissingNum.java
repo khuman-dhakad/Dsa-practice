8aae70761e1ab38b1a1bcceb348b947771cf37e7
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-21 15:35:35 +0530

    Added comments in the prograns for identify and  undrstanding of program sections

--- Arrays/FindmissingNum.java
+++ Arrays/FindmissingNum.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class FindMissingNum {
+public class FindmissingNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -9,27 +8,28 @@ public class FindMissingNum {
 
         int[] arr = new int[n];
 
+        // for taking input from user
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
+        //For checking number 0 to n
         for (int i = 0; i <= n; i++) {
-
             boolean found = false;
 
+            // For check avaiability of element
             for (int j = 0; j < n; j++) {
                 if (arr[j] == i) {
                     found = true;
                     break;
                 }
             }
+            //  If not found then missing number is
             if (!found) {
                 System.out.println("Missing number = " + i);
                 break;
             }
         }
-
         sc.close();
     }
 }
