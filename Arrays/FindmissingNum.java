d46a9f730c59ed553f200f0b05b76b56f966d104
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-21 15:21:46 +0530

    Added FindMissingNum program with the size of an array

---
+++ Arrays/FindmissingNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FindMissingNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+       
+        sc.close();
+    }
+}
