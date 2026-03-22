43ed6f8d2eec531222b436fe74dcf02564a57c22
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-22 17:03:56 +0530

    Added OddCount program

---
+++ Arrays/OddCount.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class OddCount {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+
+        sc.close();
+    }
+}
