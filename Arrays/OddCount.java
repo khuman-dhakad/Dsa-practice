8007d5a6101ba6066516174258670a4bc52970d1
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-22 17:06:50 +0530

    create array and take input from user

--- Arrays/OddCount.java
+++ Arrays/OddCount.java
@@ -8,6 +8,14 @@ public class OddCount {
         System.out.print("Enter size of array: ");
         int size = sc.nextInt();
 
+       
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
         sc.close();
     }
 }
