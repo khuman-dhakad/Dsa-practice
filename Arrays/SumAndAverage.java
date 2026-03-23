92fe99a2628ba771806ca910018c6ca57282d0ee
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-23 18:33:50 +0530

    Added loop for taking element input from user and create sum and average variable

--- Arrays/SumAndAverage.java
+++ Arrays/SumAndAverage.java
@@ -7,7 +7,15 @@ public class SumAndAverage{
         int size = sc.nextInt();
 
         int []arr = new int [size];
+
+        for(int i = 0;i<arr.length;i++){
+            System.out.print("Enter element "+(i+1)+" :");
+            arr[i]=sc.nextInt();
+        }
+        int sum = 0;
+        int average = 0;
         
+
         sc.close();
     }
 }
