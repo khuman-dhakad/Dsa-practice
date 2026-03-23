745682d76cb6e766e9246170efb350ee0a4cb4e7
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-23 18:26:49 +0530

    Added SumAndAverage program

---
+++ Arrays/SumAndAverage.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class SumAndAverage{
+    public static void main(Static[]args){
+        Scannner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
