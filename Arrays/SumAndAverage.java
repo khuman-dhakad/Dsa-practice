fb481a9d983b142f35537ee19f97cd89cabaa0dc
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-23 18:39:23 +0530

    Added loop for calculating sum and condition for average

--- Arrays/SumAndAverage.java
+++ Arrays/SumAndAverage.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class SumAndAverage{
-    public static void main(Static[]args){
-        Scannner sc = new Scanner(System.in);
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter Size : ");
         int size = sc.nextInt();
@@ -13,8 +13,16 @@ public class SumAndAverage{
             arr[i]=sc.nextInt();
         }
         int sum = 0;
-        int average = 0;
-        
+        int avg = 0;
+
+        for(int i=0;i<arr.length;i++){
+            sum = sum+arr[i];
+        }
+        System.out.println("Sum is = "+sum);
+        avg = sum/arr.length;
+
+         System.out.println("Average is = "+avg);
+
 
         sc.close();
     }
