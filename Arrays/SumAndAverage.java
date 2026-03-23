60a7ef969898f0caceb61f92a9d037598251f94b
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-23 18:42:36 +0530

    Added loop for print element and print sum and average of array

--- Arrays/SumAndAverage.java
+++ Arrays/SumAndAverage.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
-public class SumAndAverage{
-    public static void main(String[]args){
+
+public class SumAndAverage {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter Size : ");
         int size = sc.nextInt();
 
-        int []arr = new int [size];
+        int[] arr = new int[size];
 
-        for(int i = 0;i<arr.length;i++){
-            System.out.print("Enter element "+(i+1)+" :");
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter element " + (i + 1) + " :");
+            arr[i] = sc.nextInt();
         }
         int sum = 0;
         int avg = 0;
 
-        for(int i=0;i<arr.length;i++){
-            sum = sum+arr[i];
+
+         System.out.print("Array is :");
+        for (int i = 0; i < arr.length; i++) {
+             System.out.print(arr[i]+" ");
+            sum = sum + arr[i];
         }
-        System.out.println("Sum is = "+sum);
-        avg = sum/arr.length;
+        System.out.println("\nSum is = " + sum);
+        avg = sum / arr.length;
 
-         System.out.println("Average is = "+avg);
-
+        System.out.println("Average is = " + avg);
 
         sc.close();
     }
