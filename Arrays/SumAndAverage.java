705bad9635f4e47e1a19955cd38e05bbc49dfd1c
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-23 18:30:41 +0530

    Create array from take input size

--- Arrays/SumAndAverage.java
+++ Arrays/SumAndAverage.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 public class SumAndAverage{
     public static void main(Static[]args){
         Scannner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size : ");
+        int size = sc.nextInt();
+
+        int []arr = new int [size];
         
         sc.close();
     }
