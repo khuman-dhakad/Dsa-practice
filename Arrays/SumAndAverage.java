d9e8b0a1fc68690c388629727d4daaa4c6e71ae8
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-23 18:46:32 +0530

    Added comments in program for better understanding

--- Arrays/SumAndAverage.java
+++ Arrays/SumAndAverage.java
@@ -9,6 +9,7 @@ public class SumAndAverage {
 
         int[] arr = new int[size];
 
+        // For taking elemnt input from user
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Enter element " + (i + 1) + " :");
             arr[i] = sc.nextInt();
@@ -16,10 +17,10 @@ public class SumAndAverage {
         int sum = 0;
         int avg = 0;
 
-
-         System.out.print("Array is :");
+        System.out.print("Array is :");
+        // For print array element and sum claculating
         for (int i = 0; i < arr.length; i++) {
-             System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
             sum = sum + arr[i];
         }
         System.out.println("\nSum is = " + sum);
