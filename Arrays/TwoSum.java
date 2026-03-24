3a84090150ffcaf36e583400dff326f357cdd05d
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-24 16:44:37 +0530

    Create array and take size input from user

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
-public class TwoSum{
-    public static void main(String[]args){
+
+public class TwoSum {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
         sc.close();
     }
 }
