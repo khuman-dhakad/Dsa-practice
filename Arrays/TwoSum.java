9da4b6ae00b8878d353a1717e20f0ed804923ca3
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-24 17:12:15 +0530

    Added output if pair not found

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -11,19 +11,20 @@ public class TwoSum {
 
         System.out.print("Enter Target : ");
         int target = sc.nextInt();
-        for(int i=0;i<arr.length;i++){
-            System.out.print("Enter Element : "+(i+1)+" :");
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter Element : " + (i + 1) + " :");
+            arr[i] = sc.nextInt();
         }
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
-                    System.out.print(arr[i] +" "+arr[j]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.print(arr[i] + " " + arr[j]);
                     return;
                 }
             }
         }
+        System.out.println("No Pair Found!");
         sc.close();
     }
 }
