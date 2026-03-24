6093ac2a268d080e4be3f3b2dcb9db21ac040c0b
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-24 17:18:27 +0530

    Added comments for better understanding

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -11,11 +11,13 @@ public class TwoSum {
 
         System.out.print("Enter Target : ");
         int target = sc.nextInt();
+        //For taking elements from user
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Enter Element : " + (i + 1) + " :");
             arr[i] = sc.nextInt();
         }
 
+        //For find pairs of to sum
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == target) {
