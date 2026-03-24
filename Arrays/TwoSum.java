3fa29aa3e549499502dc5b7f0b9972375a1cc9f0
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-24 17:11:14 +0530

    Take element input from user and added nested loop to find two sum pair

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -6,7 +6,24 @@ public class TwoSum {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Size : ");
         int size = sc.nextInt();
+
         int[] arr = new int[size];
+
+        System.out.print("Enter Target : ");
+        int target = sc.nextInt();
+        for(int i=0;i<arr.length;i++){
+            System.out.print("Enter Element : "+(i+1)+" :");
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.print(arr[i] +" "+arr[j]);
+                    return;
+                }
+            }
+        }
         sc.close();
     }
 }
