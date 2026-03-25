657eae3b9b03562577c016280961aaa2e72ca113
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-25 20:01:15 +0530

    Added comments in program for better understanding

--- Arrays/DuplicateNumber.java
+++ Arrays/DuplicateNumber.java
@@ -1,37 +1,35 @@
 import java.util.Scanner;
-
 public class DuplicateNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter size: ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
 
         System.out.println("Enter elements:");
-        for(int i = 0; i < n; i++) {
+        // For taking array elements input from user
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
         int duplicate = -1;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(arr[i] == arr[j]) {
+        // For find duplicate number
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
                     duplicate = arr[i];
                     break;
                 }
             }
-            if(duplicate != -1) {
+            if (duplicate != -1) {
                 break;
             }
         }
-         if(duplicate != -1) {
+        // For print duplicate number
+        if (duplicate != -1) {
             System.out.println("Duplicate number is: " + duplicate);
         } else {
             System.out.println("No duplicate found");
         }
-            sc.close();
+        sc.close();
     }
 }
