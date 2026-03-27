b6175d5b3ceae63454518a74867041b865fc4203
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-27 19:43:24 +0530

    Added MoveZeros program

---
+++ Arrays/MoveZeros.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MoveZeros {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        sc.close();
+    }
+}
