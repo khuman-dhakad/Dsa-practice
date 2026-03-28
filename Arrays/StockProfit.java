f9ac520423a756f7c136d9cfad610a9b95fe3d34
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-28 18:47:15 +0530

    Take stock price input from user

--- Arrays/StockProfit.java
+++ Arrays/StockProfit.java
@@ -7,8 +7,13 @@ public class StockProfit {
 
         System.out.print("Enter size of array: ");
         int size = sc.nextInt();
-        
+
         int[] arr = new int[size];
+
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
         sc.close();
     }
 }
