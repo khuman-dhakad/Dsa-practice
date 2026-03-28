700fa5689279a8473de169949394e8e6b69ce2a6
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-28 18:51:20 +0530

    Added variable to store min and max price and add loop with condition to check price

--- Arrays/StockProfit.java
+++ Arrays/StockProfit.java
@@ -14,6 +14,22 @@ public class StockProfit {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
+
+        int minPrice = arr[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] < minPrice) {
+                minPrice = arr[i]; 
+            } else {
+                int profit = arr[i] - minPrice;
+
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
         sc.close();
     }
 }
