117da17f8c9ae721c733ef94d9b4a948286fc768
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-28 18:42:43 +0530

    Added StockProfit program

--- Arrays/MoveZeros.java
+++ Arrays/MoveZeros.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class MoveZeros {
     public static void main(String[] args) {
 
@@ -9,30 +8,28 @@ public class MoveZeros {
         int size = sc.nextInt();
 
         int[] arr = new int[size];
-
+        // For taking array elements from user
         System.out.println("Enter elements:");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-
         int j = 0;
-
-        
+        // For zeros movements
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
                 arr[j] = arr[i];
                 j++;
             }
         }
-
-        
+        // For filling remaining zeros
         for (int i = j; i < arr.length; i++) {
             arr[i] = 0;
         }
         System.out.println("Array after moving zeros:");
+        // For print result
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        sc.close(); 
+        sc.close();
     }
 }
