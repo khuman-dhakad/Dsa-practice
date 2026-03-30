6dd1d814f080a97fb683330c7e4c036f0cdf2521
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-30 18:43:12 +0530

    Added loop with condition to find MaximumSubarray

--- Arrays/MaximumSubarray .java
+++ Arrays/MaximumSubarray .java
@@ -17,6 +17,20 @@ public class MaximumSubarray {
 
         int currentSum = arr[0];
         int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (currentSum + arr[i] > arr[i]) {
+                currentSum = currentSum + arr[i];
+            } else {
+                currentSum = arr[i];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
         sc.close();
     }
 }
