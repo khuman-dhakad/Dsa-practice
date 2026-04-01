f79818f14fd0159fccd72aedee56eb38d4b21504
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-01 21:28:54 +0530

    Added variables to store values and add for loop to find element

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -13,6 +13,22 @@ public class MajorityElement {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
+        int candidate = arr[0];
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+
+            if (count == 0) {
+                candidate = arr[i];
+                count = 1;
+            }
+        }
         sc.close();
     }
 }
