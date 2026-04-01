6df646a618e9a6482d6e8a771aced41fdfa054b2
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-01 21:25:43 +0530

    Added for loop to take input of array element

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -4,11 +4,15 @@ public class MajorityElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input size
         System.out.print("Enter size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         sc.close();
     }
 }
