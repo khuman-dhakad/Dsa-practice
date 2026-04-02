387888d5acf4409d2b978d350ed0e6b50db721fb
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-02 22:44:06 +0530

    Create array and take array size from user

--- Arrays/SubarraySumK.java
+++ Arrays/SubarraySumK.java
@@ -3,6 +3,12 @@ import java.util.*;
 public class SubarraySumK {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
         sc.close();
     }
 }
