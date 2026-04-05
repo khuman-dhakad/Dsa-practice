b8d59bad56839d2c2ca2e82da952ad65e1aa2e0b
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-05 23:21:58 +0530

    Added comments for better understanding

--- Arrays/ProductExceptSelf.java
+++ Arrays/ProductExceptSelf.java
@@ -4,34 +4,33 @@ public class ProductExceptSelf {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Input size
         System.out.print("Enter size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-
+        // Input elements
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
         int[] result = new int[n];
-
+        //  Left product
         result[0] = 1;
         for (int i = 1; i < n; i++) {
             result[i] = result[i - 1] * arr[i - 1];
         }
-
+        //  Right product
         int right = 1;
         for (int i = n - 1; i >= 0; i--) {
             result[i] = result[i] * right;
             right *= arr[i];
         }
-
+        // Output
         System.out.println("Result array:");
         for (int num : result) {
             System.out.print(num + " ");
         }
-
         sc.close();
     }
 }
