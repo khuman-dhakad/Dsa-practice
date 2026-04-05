fdddfcdb2b79885f77685b326b7480f3cb4e9c4c
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-05 23:13:32 +0530

    Add one more array for result and add conditions

--- Arrays/ProductExceptSelf.java
+++ Arrays/ProductExceptSelf.java
@@ -13,7 +13,19 @@ public class ProductExceptSelf {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        
+
+        int[] result = new int[n];
+
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * arr[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right *= arr[i];
+        }
         sc.close();
     }
 }
