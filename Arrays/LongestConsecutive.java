cf6c6530a701359a09242e248cb0f09a87bf800d
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-07 22:34:13 +0530

    Create array and take size from user

--- Arrays/LongestConsecutive.java
+++ Arrays/LongestConsecutive.java
@@ -4,6 +4,11 @@ public class LongestConsecutive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
         sc.close();
     }
 }
