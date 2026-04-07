a05cbb1f70e78c56c0f224640e9e574989d871e6
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-07 22:36:05 +0530

    Added forloop and take array elements input from user

--- Arrays/LongestConsecutive.java
+++ Arrays/LongestConsecutive.java
@@ -9,6 +9,10 @@ public class LongestConsecutive {
 
         int[] arr = new int[n];
 
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         sc.close();
     }
 }
