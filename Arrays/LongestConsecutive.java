1e7c6d0ee72b072b55bda4bc88bfcc300e755745
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-08 19:35:20 +0530

    Added comments in program for better understanding

--- Arrays/LongestConsecutive.java
+++ Arrays/LongestConsecutive.java
@@ -4,16 +4,19 @@ public class LongestConsecutive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Input size
         System.out.print("Enter size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
+        // Input elements
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
+        // HashSet
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             set.add(arr[i]);
@@ -21,13 +24,16 @@ public class LongestConsecutive {
 
         int longest = 0;
 
+        // Traverse using basic loop
         for (int i = 0; i < n; i++) {
             int num = arr[i];
 
+            // Check start of sequence
             if (!set.contains(num - 1)) {
                 int current = num;
                 int count = 1;
 
+                // Count sequence
                 while (set.contains(current + 1)) {
                     current++;
                     count++;
@@ -39,6 +45,7 @@ public class LongestConsecutive {
             }
         }
 
+        // Output
         System.out.println("Longest consecutive length: " + longest);
 
         sc.close();
