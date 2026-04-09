43b9f9615b9073a49e884ac366a60471c570b9fc
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-09 23:14:57 +0530

    Create array and take from user

--- Arrays/TrappingRainWater.java
+++ Arrays/TrappingRainWater.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class TrappingRainWater {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
         sc.close();
     }
 }
