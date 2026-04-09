223fc2830feb44b7ad61ae023995a7854b24a721
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-09 23:16:40 +0530

    Take array elements input from user

--- Arrays/TrappingRainWater.java
+++ Arrays/TrappingRainWater.java
@@ -9,6 +9,15 @@ public class TrappingRainWater {
 
         int[] arr = new int[n];
 
+        System.out.println("Enter heights:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
         sc.close();
     }
 }
