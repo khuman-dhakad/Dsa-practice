9bb257a418d4621b13416f57a12813f28da282a6
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-11 20:41:39 +0530

    Add comments to explain Jump Game logic

--- Arrays/JumpGame.java
+++ Arrays/JumpGame.java
@@ -4,11 +4,13 @@ public class JumpGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Input size
         System.out.print("Enter size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
+        // Input elements
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
@@ -17,17 +19,20 @@ public class JumpGame {
         int maxReach = 0;
 
         for (int i = 0; i < n; i++) {
+            // If current index is not reachable
             if (i > maxReach) {
                 System.out.println(false);
                 sc.close();
                 return;
             }
 
+            // Update max reach
             if (i + arr[i] > maxReach) {
                 maxReach = i + arr[i];
             }
         }
 
+        // If loop completes then last index reachable
         System.out.println(true);
 
         sc.close();
