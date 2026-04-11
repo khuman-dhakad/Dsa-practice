a28a4be389b29297ca6d2bf896c3ec2af95dcf13
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-11 20:34:13 +0530

    Create array and take array size and elements input from user

--- Arrays/JumpGame.java
+++ Arrays/JumpGame.java
@@ -4,6 +4,17 @@ public class JumpGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
         sc.close();
     }
 }
