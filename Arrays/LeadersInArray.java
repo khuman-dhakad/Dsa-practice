feb12958148b92c813536b827dc9785e65b3d2d1
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-12 19:02:35 +0530

    Added comments in program for better understanding

--- Arrays/LeadersInArray.java
+++ Arrays/LeadersInArray.java
@@ -4,11 +4,13 @@ public class LeadersInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Input size
         System.out.print("Enter size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
+        // Input elements
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
@@ -16,9 +18,11 @@ public class LeadersInArray {
 
         System.out.println("Leaders:");
 
+        // Last element is always leader
         int max = arr[n - 1];
         System.out.print(max + " ");
 
+        // Traverse from right to left
         for (int i = n - 2; i >= 0; i--) {
             if (arr[i] > max) {
                 System.out.print(arr[i] + " ");
