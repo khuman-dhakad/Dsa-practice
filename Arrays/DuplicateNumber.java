aa7971e0d86f20e75290b2c10a2b472d266d4e74
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-18 23:02:22 +0530

    Sort array and fix sorted condition issue

--- Arrays/DuplicateNumber.java
+++ Arrays/DuplicateNumber.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class DuplicateNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter size: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
 
         System.out.println("Enter elements:");
-        // For taking array elements input from user
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
+        Arrays.sort(arr);
+
         int duplicate = -1;
-        
-          for (int i = 1; i < n; i++) {
+
+        for (int i = 1; i < n; i++) {
             if (arr[i] == arr[i - 1]) {
                 duplicate = arr[i];
                 break;
             }
         }
-        // For print duplicate number
+
         if (duplicate != -1) {
             System.out.println("Duplicate number is: " + duplicate);
         } else {
             System.out.println("No duplicate found");
         }
+
         sc.close();
     }
 }
