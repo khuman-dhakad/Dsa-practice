11fc0978310b6d3e8f32426d02ce4806c49315a3
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-23 21:51:09 +0530

    Part 4: Implemented equilibrium index logic using prefix sum

--- Arrays/EquilibriumIndex.java
+++ Arrays/EquilibriumIndex.java
@@ -7,16 +7,33 @@ public class EquilibriumIndex {
 
         System.out.print("Enter size: ");
         int n = sc.nextInt();
-                int[] arr = new int[n];
+
+        int[] arr = new int[n];
 
         System.out.println("Enter elements:");
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            
         }
-                int totalSum = 0;
+
+        int totalSum = 0;
         for(int i = 0; i < n; i++) {
             totalSum += arr[i];
         }
+
+        int leftSum = 0;
+        int index = -1;
+
+        for(int i = 0; i < n; i++) {
+
+            int rightSum = totalSum - leftSum - arr[i];
+
+            if(leftSum == rightSum) {
+                index = i;
+                break;
+            }
+
+            leftSum += arr[i];
+        }
+        System.out.println(index);
     }
 }
