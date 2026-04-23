92c37f29f908ad0c06594a12e56407b770d0ba00
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-23 21:47:23 +0530

    Part 2: Added array creation and user input

--- Arrays/EquilibriumIndex.java
+++ Arrays/EquilibriumIndex.java
@@ -7,5 +7,11 @@ public class EquilibriumIndex {
 
         System.out.print("Enter size: ");
         int n = sc.nextInt();
+                int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
     }
 }
