3340be6f25b9c2856e0cbbfe11f006822d4b4c53
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-23 21:46:29 +0530

    Part 1: Setup class and take array size input

---
+++ Arrays/EquilibriumIndex.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EquilibriumIndex {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+    }
+}
