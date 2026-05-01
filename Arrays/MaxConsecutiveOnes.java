ed8dfe16b0f981896c50f337afae1eae88219f83
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-01 20:56:16 +0530

    Part 2: Add input handling using Scanner and array

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class MaxConsecutiveOnes {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements (0 or 1): ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
     }
 }
