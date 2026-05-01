99ec78ee76aadde1e2686835cb80ec030b6e690f
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-01 21:07:14 +0530

    Part 5: Final cleanup and minor improvements

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -4,29 +4,31 @@ public class MaxConsecutiveOnes {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+        //input size
         System.out.print("Enter size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-
+        //input element
         System.out.println("Enter elements (0 or 1): ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        int count = 0;
-        int maxCount = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 1) {
-                count++;
-            if (count > maxCount) {
-            maxCount = count;
-        }
-    } else {
-        count = 0;
-    }
-}
-    System.out.println("Maximum consecutive 1s = " + maxCount);
+                int count = 0;
+                int maxCount = 0;
+                    // Single pass
+             for (int i = 0; i < n; i++) {
+                if (arr[i] == 1) {
+                       count++;
+                      if (count > maxCount) {
+                        maxCount = count;
+                    }
+                         } else {
+                         count = 0;
+                    }
+                }
+            //output
+            System.out.println("Maximum consecutive 1s = " + maxCount);
+            sc.close();
     }
 }
