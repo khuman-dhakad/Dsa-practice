5e20ae548cd6c40a71ba6b4adc0a8d7f52ef7e4f
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-05 11:08:10 +0530

    Swap pivot with next greater element on right side

--- Arrays/NextPermutation.java
+++ Arrays/NextPermutation.java
@@ -19,5 +19,16 @@ public class NextPermutation {
         while(i >= 0 && arr[i] >= arr[i + 1]) {
             i--;
         }
+                if(i >= 0) {
+            int j = n - 1;
+
+            while(arr[j] <= arr[i]) {
+                j--;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
     }
 }
