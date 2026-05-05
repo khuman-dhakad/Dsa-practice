7e658792cfa872fcc69a6f0088757bd002ec6741
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-05 11:07:30 +0530

    Read array elements from user input

--- Arrays/NextPermutation.java
+++ Arrays/NextPermutation.java
@@ -7,5 +7,12 @@ public class NextPermutation {
 
         System.out.print("Enter size: ");
         int n = sc.nextInt();
+
+                int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
     }
 }
