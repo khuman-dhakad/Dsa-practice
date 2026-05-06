f3a56add5244967c65369b28f660671239e18f5f
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-06 12:34:14 +0530

    Store interval start and end values from user input

--- Arrays/MergeIntervals.java
+++ Arrays/MergeIntervals.java
@@ -7,5 +7,14 @@ public class MergeIntervals {
 
         System.out.print("Enter number of intervals: ");
         int n = sc.nextInt();
+        
+        int[][] intervals = new int[n][2];
+
+        System.out.println("Enter intervals:");
+
+        for(int i = 0; i < n; i++) {
+            intervals[i][0] = sc.nextInt();
+            intervals[i][1] = sc.nextInt();
+        }
     }
 }
