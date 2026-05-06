e0593c5090f2d5efb114ab1a5b0f74bfe98c0985
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-06 12:33:42 +0530

    Initialize program and read number of intervals

---
+++ Arrays/MergeIntervals.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of intervals: ");
+        int n = sc.nextInt();
+    }
+}
