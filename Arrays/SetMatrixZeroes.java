4c921d7492ce97857f4165885a9229b9ea5d1089
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-07 20:33:18 +0530

    Initialize program and read matrix dimensions

---
+++ Arrays/SetMatrixZeroes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SetMatrixZeroes {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter columns: ");
+        int cols = sc.nextInt();
+    }
+}
