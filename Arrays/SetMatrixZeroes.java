36713678282b2854d71c0f04111314f1641598c7
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-07 20:36:22 +0530

    Display final matrix after applying zero transformation

--- Arrays/SetMatrixZeroes.java
+++ Arrays/SetMatrixZeroes.java
@@ -40,5 +40,13 @@ public class SetMatrixZeroes {
                 }
             }
         }
+        System.out.println("Final Matrix:");
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
