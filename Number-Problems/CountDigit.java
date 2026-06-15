0a2b9a601f22e0c2976243f3f4f3708c9b431805
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-15 20:26:40 +0530

    Take nubmer from user and store it in num variable

--- Number-Problems/CountDigit.java
+++ Number-Problems/CountDigit.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class CountDigit{
     public static void main(String[] args){
-        
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter num : ");
+      int num = sc.nextInt();
     }
 }
