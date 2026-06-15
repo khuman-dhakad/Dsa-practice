8116c632536eb1a8f8b0792eba6d20d4b43ac9d9
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-15 20:32:34 +0530

    Fixed class bug and write logic in method and return count

--- Number-Problems/CountDigit.java
+++ Number-Problems/CountDigit.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
-public static int countDigit(int num){
-    
-}
 public class CountDigit{
+public static int countDigit(int num){
+    int count = 0;
+    while(num != 0){
+        num/=10;
+        count++;
+    }
+    return count;
+}
     public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter num : ");
