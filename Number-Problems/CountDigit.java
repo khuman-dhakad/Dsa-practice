90239999262e2f3dcdc4b31e6348004bd535f729
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-15 20:28:00 +0530

    Create a new method to count digits of number

--- Number-Problems/CountDigit.java
+++ Number-Problems/CountDigit.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+public static int countDigit(int num){
+    
+}
 public class CountDigit{
     public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
