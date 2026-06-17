eded4e5b8ea8ab13acf624539d43bbfcd09ae2cb
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-17 22:19:33 +0530

    Create new method sumOfDigit to calculate sum

--- Number-Problems/SumOfDigit.java
+++ Number-Problems/SumOfDigit.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 public class SumOfDigit{
+    public static int sumOfDigit(int num){
+        
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter num : ");
         int num = sc.nextInt();
-        
+
     }
 }
