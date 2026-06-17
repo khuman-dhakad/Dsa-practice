c046f7d45a99e5507274afde8f0e0633aff87f41
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-17 22:18:13 +0530

    Take number input from user and store in num variable

--- Number-Problems/SumOfDigit.java
+++ Number-Problems/SumOfDigit.java
@@ -2,5 +2,8 @@ import java.util.Scanner;
 public class SumOfDigit{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter num : ");
+        int num = sc.nextInt();
+        
     }
 }
