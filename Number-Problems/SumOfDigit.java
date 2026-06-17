e32fdc638443b1d80a32537b2bd6ed5facb1a9e0
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-17 22:23:17 +0530

    Write logic to Calculate sum and return sum

--- Number-Problems/SumOfDigit.java
+++ Number-Problems/SumOfDigit.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 public class SumOfDigit{
     public static int sumOfDigit(int num){
-        
+        int sum = 0;
+        while(num != 0){
+            int digit = num % 10;
+            sum = sum+digit;
+            num/=10;
+        }
+        return sum;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter num : ");
         int num = sc.nextInt();
+        
 
     }
 }
