b840ff9c1b533c9ec390038663d0caf19c86e30d
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-18 19:43:20 +0530

    Create new method to reverse number

--- Number-Problems/ReverseNum.java
+++ Number-Problems/ReverseNum.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 public class ReverseNum{
+    public static int reverseNum(int num){
+        
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number : ");
