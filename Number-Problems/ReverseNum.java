77a921c6f0b2d87014bb6fbe14b3f046b9e8f64b
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-18 19:46:15 +0530

    Reverse number and store it in rev variable and return it to main method

--- Number-Problems/ReverseNum.java
+++ Number-Problems/ReverseNum.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
 public class ReverseNum{
     public static int reverseNum(int num){
-        
+        int rev = 0;
+        while(num != 0){
+            int digit = num % 10;
+            rev = (rev * 10)+digit;
+            num/=10;
+        }
+        return rev;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
