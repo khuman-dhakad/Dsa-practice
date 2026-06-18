d519c4226a442a655a48ccbde66a272c77bd2782
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-18 19:42:07 +0530

    Take numbers input from user and store it in num variable

--- Number-Problems/ReverseNum.java
+++ Number-Problems/ReverseNum.java
@@ -2,5 +2,7 @@ import java.util.Scanner;
 public class ReverseNum{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int num = sc.nextInt();
     }
 }
