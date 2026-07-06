51bda191c7b2d240766793a2458ecc77cc632ce2
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-06 18:21:53 +0530

    Implement Euclidean algorithm for GCD

--- Number-Problems/GCD.java
+++ Number-Problems/GCD.java
@@ -1,7 +1,12 @@
 public class GCD {
 
     public static int gcd(int a, int b) {
-        return 0;
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
     public static void main(String[] args) {
