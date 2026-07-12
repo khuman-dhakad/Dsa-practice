503ad32b2b2c22aff6a79f4c77ba776e63c8ee47
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-12 20:25:20 +0530

    Add logic to find PowerOfNum

--- Number-Problems/powerOfNum.java
+++ Number-Problems/powerOfNum.java
@@ -1,5 +1,11 @@
-public class PownerOfNum{
-    public static void main(String[]args){
+ class PowerOfNum {
+    public static void main(String[] args) {
+        double base = 5.0;
+        double exponent = 3.0;
         
+        // Calculating the power using Math.pow
+        double result = Math.pow(base, exponent);
+        
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
     }
 }
