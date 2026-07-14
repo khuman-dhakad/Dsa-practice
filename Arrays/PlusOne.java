c72a5caed6306cdca926de8e4237a801b506329d
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-14 21:17:57 +0530

    Added LeetCode 66: Plus One solution

---
+++ Arrays/PlusOne.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        // Traverse from the last digit
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
