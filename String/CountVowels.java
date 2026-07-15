fc8bd23f84e0c1191bf227a0eff86fd1371f2013
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-15 22:50:12 +0530

    Added Countvowels progrm

---
+++ String/CountVowels.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        System.out.println("Input received: " + input);
+        scanner.close();
+    }
+}
