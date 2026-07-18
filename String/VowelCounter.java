5f75b396f9fed9c4f418f92f844b46b69b747b64
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-18 13:45:26 +0530

    Step 2: Implement vowel counting logic method

--- String/VowelCounter.java
+++ String/VowelCounter.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class VowelCounter {
+    public static int countVowels(String text) {
+        String vowels = "aeiouAEIOU";
+        int count = 0;
+        for (char c : text.toCharArray()) {
+            if (vowels.indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
