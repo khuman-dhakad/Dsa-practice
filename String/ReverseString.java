035c8e0f846806727623e8dc1b62e72b34c7718f
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-19 17:35:02 +0530

    Take string input from user and store it also created new reversed string

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class ReverseString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string to reverse: ");
+        String original = scanner.nextLine();
+        
+        String reversed = "";
         scanner.close();
     }
 }
