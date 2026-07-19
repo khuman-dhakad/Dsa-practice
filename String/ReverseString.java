148737f02710ac784d9577cd0a3502cd2545fe88
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-19 17:37:15 +0530

    Added comments in program for better understanding

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
+        // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
         
         System.out.print("Enter a string to reverse: ");
@@ -9,12 +10,14 @@ public class ReverseString {
         
         String reversed = "";
         
+        // Loop backward from the last character to the first
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
         
         System.out.println("Reversed string: " + reversed);
         
+        // Close the scanner resource
         scanner.close();
     }
 }
