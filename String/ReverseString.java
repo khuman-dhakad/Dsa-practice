f2e70efa80e27c151c3fedb9f2269a777945f540
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-19 17:36:28 +0530

    Reverse string using for loop and print it

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -8,6 +8,13 @@ public class ReverseString {
         String original = scanner.nextLine();
         
         String reversed = "";
+        
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        
+        System.out.println("Reversed string: " + reversed);
+        
         scanner.close();
     }
 }
