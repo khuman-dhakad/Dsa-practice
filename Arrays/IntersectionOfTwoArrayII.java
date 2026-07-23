4a0ec2d05d6aa0d4ae74acb2a27f6b9f603b291f
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-23 20:14:42 +0530

    Add multiple test cases for intersection solution

--- Arrays/IntersectionOfTwoArrayII.java
+++ Arrays/IntersectionOfTwoArrayII.java
@@ -47,9 +47,13 @@ public class IntersectionOfTwoArrayII {
         int[] nums1 = {1, 2, 2, 1};
         int[] nums2 = {2, 2};
 
-        int[] result = solution.intersect(nums1, nums2);
+        System.out.println(Arrays.toString(solution.intersect(nums1, nums2)));
 
-        System.out.println(Arrays.toString(result));
+        int[] nums3 = {4, 9, 5};
+        int[] nums4 = {9, 4, 9, 8, 4};
+
+        System.out.println(Arrays.toString(solution.intersect(nums3, nums4)));
+
     }
 
 }
