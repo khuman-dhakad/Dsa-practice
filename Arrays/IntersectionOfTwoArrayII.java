9b2995196c2f45900d3b281bba121592a221acc2
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-07-23 20:09:32 +0530

    Added IntersectionOfTwoArrayII program

---
+++ Arrays/IntersectionOfTwoArrayII.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // TODO: Implement intersection logic
+        return new int[0];
+    }
+
+}
+
+public class  IntersectionOfTwoArrayII{
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+
+        int[] result = solution.intersect(nums1, nums2);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+}
