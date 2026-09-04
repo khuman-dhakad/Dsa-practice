601b927190bae6539805d4561f977ad12e7312a7
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-09-04 22:16:52 +0530

    Add MaximumProduct class skeleton
    Add the runnable class and entry point for the maximum product problem.

---
+++ Number-Problems/MaximumProduct.java
@@ -0,0 +1,12 @@
+public class MaximumProduct {
+
+    public int maximumProduct(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        MaximumProduct solver = new MaximumProduct();
+        int[] test1 = {1, 2, 3, 4};
+        System.out.println(solver.maximumProduct(test1));
+    }
+}
